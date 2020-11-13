425e21009bb0335aa4cfed5638f52d3ba0326857
brunoestivalli <bruno.estivalli@gmail.com>
2020-11-13 00:57:45 -0300

    exercicio 3 correto

--- java/ListaRevisao/src/Exercicio3.java
+++ java/ListaRevisao/src/Exercicio3.java
@@ -8,32 +8,28 @@ public class Exercicio3 {
 	public static void main(String[] args) {
 		Scanner read=new Scanner(System.in);
 		int x=0,x1=0,x2=0,x3=0,x4=0;
-		boolean confirmar=true;
 		
-		do {
+		
+		while(x>=0) {
 			System.out.println("\nDigite um número: ");
 			x=read.nextInt();
-			if(x>0) {
-				confirmar=true;
-			}else if(x<0) {
-				confirmar=false;
 			
-			}else if(x>0 && x<=25) {
-				x1=x1++;
+			if(x>0 && x<=25) {
+				x1++;
 			}else if(x>=26 && x<=50) {
-				x2=x2++;
+				x2++;
 			}else if(x>=51 && x<=75) {
-				x3=x3++;
+				x3++;
 			}else if(x>=76 && x<=101) {
-				x4=x4++;
+				x4++;
 			}
 			System.out.printf("\nEntre os numeros 0~25: %d",x1);
 			System.out.printf("\nEntre os numeros 26~50: %d",x2);
 			System.out.printf("\nEntre os numeros 51~75: %d",x3);
 			System.out.printf("\nEntre os numeros 76~100: %d",x4);
+		}
 			
 			
-	
-		}while(confirmar==true);
+		
 	}
 }
