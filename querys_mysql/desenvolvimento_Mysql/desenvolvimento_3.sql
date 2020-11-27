0ceeba44d93b289fa08391ebc6b5d183a2e27934
brunoestivalli <bruno.estivalli@gmail.com>
2020-11-27 14:33:02 -0300

    desenvolvimento 3 correto

--- querys_mysql/desenvolvimento_Mysql/desenvolvimento_3.sql
+++ querys_mysql/desenvolvimento_Mysql/desenvolvimento_3.sql
@@ -1,31 +1,32 @@
 create database db_pizzaria_legal;
 use db_pizzaria_legal;
 
-create table tb_categorias0(
+create table tb_categorias(
 
 	idcategoria integer PRIMARY key,
     disponiveis varchar(100));
     
-create table tb_pizza0(
+create table tb_pizza(
 
 	idPizza integer PRIMARY KEY KEY AUTO_INCREMENT,
 	nome varchar(100),
     preco double(6,2),
     sabor varchar(100),
     tamanho varchar(100),
-	constraint fk_categorias0 FOREIGN KEY (idPizza) REFERENCES tb_categorias0 (idcategoria));
+    idCat int,
+	constraint fk_categorias FOREIGN KEY (idCat) REFERENCES tb_categorias (idcategoria));
    
-   insert into tb_categorias0 (idcategoria,disponiveis) values 
+   insert into tb_categorias (idcategoria,disponiveis) values 
 	(1,'Salgada'),
     (2,'Doce'),
     (3,'Vegana'),
     (4,'Especial da Casa'),
     (5,'Agridoce');
     
-    select * from tb_categorias0;
+    select * from tb_categorias;
 
     
-    insert into tb_pizza0 (nome, preco, sabor, tamanho, idcategoria) values
+    insert into tb_pizza (nome, preco, sabor, tamanho, idCat) values
     ('Bruno Estivalli', 55.60, 'Marguerita','Grande',1), 
 	('Carlos Alberto', 103.54, 'Brigadeiro','Extra Grande',2), 
 	('Vitor Arthur', 25.68, 'Calabresa','Brotinho',1), 
@@ -35,14 +36,17 @@ create table tb_pizza0(
 	('Camilla Guerra', 43.65, 'Brigadeiro com Morango','Grande',2), 
 	('Abiner Rodrigo', 42.60, '4 queijos','Grande',1);
     
-    select * from tb_pizza0;
+    select * from tb_pizza;
     
-    select * from tb_pizza0 where preco > 45;
+    select * from tb_pizza where preco > 45;
     
-    select * from tb_categorias0
-    inner join tb_categorias0
-	ON tb_pizza0.idPizza=tb_categorias0.idcategoria;
+    select * from tb_categorias
+    inner join tb_pizza
+	ON tb_pizza.idCat=tb_categorias.idcategoria;
     
     select * from tb_pizza0 where idcategoria =2;
+    
+    drop table tb_pizza;
+     drop table tb_categorias;
     
 	
